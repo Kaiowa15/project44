f8e9835b087840442087194be5de52580a6f4b56
Kaiowa15 <93450381+Kaiowa15@users.noreply.github.com>
2022-04-13 19:38:51 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
-var bg, bgImg;
-var bottomGround,topGround,leftWall,rightWall;
-var spaceShip,spaceShipImg;
-var bullet,bullet1,bulletImg,bulletGrop;
+var bg, bgImg
+var bottomGround,topGround,leftWall,rightWall
+var spaceShip,spaceShipImg
+var bullet,bullet1,bulletImg,bulletGrop
+var playerHitBox
+var gameOver, restart
+var gameOverImg, restartImg
+var gameState = "play"
+
 
 
 function preload(){
@@ -9,6 +14,8 @@ function preload(){
   bgImg = loadImage("./assets/background.jpg");
   spaceShipImg = loadImage("./assets/spaceship.png")
   bulletImg = loadImage("./assets/bullet.png")
+  gameOverImg = loadImage("./assets/gameOver.png")
+  restartImg = loadImage("./assets/restart.png")
 }
 
 function setup(){
@@ -17,6 +24,19 @@ createCanvas(600, 700)
 
 spaceShip = createSprite(300,650,50,50)
 spaceShip.addImage("zuumm", spaceShipImg)
+
+playerHitBox = createSprite(300,650,1,1)
+
+gameOver = createSprite(300,350,100,200)
+gameOver.addImage("F", gameOverImg)
+gameOver.scale = 5
+gameOver.visible = false
+
+
+restart = createSprite(300,550,60,50)
+restart.addImage("oh boy here we go again", restartImg)
+restart.scale = 3
+restart.visible = false
 
 bottomGround = createSprite(300,700,600,10)
 bottomGround.visible = false
@@ -37,33 +57,71 @@ function draw() {
   
   background(bgImg);
 
-  if(keyDown(UP_ARROW)){
-   spaceShip.y -= 5
+
+  if(gameState === "play"){
+
+   if(keyDown(UP_ARROW)){
+     spaceShip.y -= 6
+   
+    }
+   
+   if(keyDown(DOWN_ARROW)){
+     spaceShip.y += 6
+    
+    }
+   
+   if(keyDown(LEFT_ARROW)){
+     spaceShip.x -= 6
+  
+    }
+   
+   if(keyDown(RIGHT_ARROW)){
+     spaceShip.x += 6
+    
+    }
+   
+   spaceShip.collide(bottomGround)
+   spaceShip.collide(topGround)
+   spaceShip.collide(leftWall)
+   spaceShip.collide(rightWall)
+
+   playerHitBox.position.x = spaceShip.position.x
+   playerHitBox.position.y = spaceShip.position.y
+
+   if(playerHitBox.isTouching(bulletGrop)){
+    gameState = "end"
+
+   }
+   
+    bulletGroup()  
 
   }
 
-  if(keyDown(DOWN_ARROW)){
-    spaceShip.y += 5
+  if(gameState === "end"){
+  spaceShip.x = 300
+  spaceShip.y = 650
+
+  playerHitBox.x = 300
+  playerHitBox.y = 650
+
+  bulletGrop.setVelocityYEach(0);
+  bulletGrop.setLifetimeEach(-1);
  
+  gameOver.visible = true
+  restart.visible = true
+
+
+  if(mousePressedOver(restart)) {
+    reset()
+
+
+   }
+
   }
 
-  if(keyDown(LEFT_ARROW)){
-    spaceShip.x -= 5
- 
-  }
 
-  if(keyDown(RIGHT_ARROW)){
-    spaceShip.x += 5
- 
-  }
-
-  spaceShip.collide(bottomGround)
-  spaceShip.collide(topGround)
-  spaceShip.collide(leftWall)
-  spaceShip.collide(rightWall)
-
+  
   drawSprites(); 
-  bulletGroup()   
 }
 
 function bulletGroup() {
@@ -75,12 +133,23 @@ function bulletGroup() {
    bullet.velocity.y = 8
    bullet.lifetime = 100;
 
-
+   bulletGrop.add(bullet)
 
 
 
 
   }
 
+}
 
+function reset(){
+  gameState = "play"
+ 
+  bulletGrop.destroyEach();
+ 
+  restart.visible = false;
+  gameOver.visible = false;
+ 
+  //score = 0  
+ 
 }
